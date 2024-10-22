e7276d353be22ceeaeb77fed2ed648eae66489d2
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-10-22 11:26:31 -0400

    Log error and value of the counter

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ function incrementVariable(number) {
 try {
     incrementVariable(0)
 } catch (error) {
-    console.error(error)
+    console.error("An error has occurred!", error)
+    console.log("Value of the counter:", counter)
 }
 
