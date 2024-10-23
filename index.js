0f98cbc76e8b1d4b137e24812621ff1198cb4faa
Ian Van Winkle <ianxvanwinkle@gmail.com>
2024-10-22 21:22:28 -0400

    Recursive Function

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+// -------------------Part 1--------------------
+
+// let counter = 0
+
+// function incrementVariable(number) {
+//     console.log(number)
+//     if (number < 15000) {
+//         incrementVariable(number+1)
+//     }
+// }
+// try {
+//     incrementVariable(0)
+// } catch (error) {
+//     console.error("An error has occurred!", error)
+//     console.log("Value of the counter:", counter)
+// }
+
+// -------------------Part 2---------------------
+
 let counter = 0
 
-function incrementVariable(number) {
-    console.log(number)
-    if (number < 15000) {
-        incrementVariable(number+1)
+const incrementVariable = (n) => {
+    console.log(n)
+    if (n < 15000) {
+        return () => incrementVariable(n + 1)
     }
 }
+
 try {
     incrementVariable(0)
 } catch (error) {
@@ -13,3 +33,24 @@ try {
     console.log("Value of the counter:", counter)
 }
 
+// ----------------Part 2 Example------------------
+
+// const factorial = (n) => {
+//     if (n === 0) return 1;
+//     return n * factorial(n - 1);
+// }
+
+// const facto = (n, a = 1) => {
+//     if (n === 0) return a;
+//     return () => facto(n - 1, n * a);
+// }
+
+// const trampoline = (f, ...args) => {
+//     let result = f(...args);
+//     while (typeof result === "function") {
+//         result = result();
+//     }
+//     return result;
+// }
+
+// console.log(trampoline(facto(10000)))
